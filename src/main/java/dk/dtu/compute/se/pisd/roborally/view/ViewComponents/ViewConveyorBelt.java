ee439d0c17adfd11a7a111d3d45786daa4875fd4
Yusuf Kara <yusufkarasan@hotmail.com>
2022-06-04 11:09:29 +0200

    Now there are images of conveyor belts. They still works as intended.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/ViewComponents/ViewConveyorBelt.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/ViewComponents/ViewConveyorBelt.java
@@ -3,43 +3,37 @@ package dk.dtu.compute.se.pisd.roborally.view.ViewComponents;
 import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.SpaceComponents.ConveyorBelt;
-import dk.dtu.compute.se.pisd.roborally.model.SpaceComponents.Wall;
 import dk.dtu.compute.se.pisd.roborally.view.SpaceView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-
-
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public class ViewConveyorBelt {
 
     public static void insertConveyorBeltView(SpaceView spaceView, FieldAction fieldAction) {
         ConveyorBelt conveyorBelt = (ConveyorBelt) fieldAction;
+        Heading heading = conveyorBelt.getHeading();
         spaceView.getChildren().clear();
-
-        Rectangle rectangle = new Rectangle(50, 50);
-        Polygon arrow = new Polygon(0.0, 0.0,
-                15.0, 30.0,
-                30.0, 0.0);
-        double v = 0;
-
-        switch (conveyorBelt.getHeading()) {
-            case SOUTH -> v = 0;
-            case WEST -> v = 90;
-            case NORTH -> v = 180;
-            case EAST -> v = 270;
-        }
-
-        arrow.setRotate(v);
-
         try {
-            arrow.setFill(Color.BLACK);
-            rectangle.setFill(Color.GREEN);
-        } catch (Exception e) {
-            arrow.setFill(Color.BLACK);
-            rectangle.setFill(Color.GREEN);
+            Image conveyorPointRight = new Image("Images/conveyorPointRight.png", 50, 50, true, true);
+            Image conveyorPointLeft = new Image("Images/conveyorPointLeft.png", 50, 50, true, true);
+            Image conveyorPointUp = new Image("Images/conveyorPointUp.png", 50, 50, true, true);
+            Image conveyorPointDown = new Image("Images/conveyorPointDown.png", 50, 50, true, true);
+
+            Canvas canvas = new Canvas(SpaceView.SPACE_WIDTH, SpaceView.SPACE_HEIGHT);
+            GraphicsContext GraphicsContext = canvas.getGraphicsContext2D();
+
+            switch (heading) {
+                case EAST -> GraphicsContext.drawImage(conveyorPointRight, 0, 0);
+                case WEST -> GraphicsContext.drawImage(conveyorPointLeft, 0, 0);
+                case NORTH -> GraphicsContext.drawImage(conveyorPointUp, 0, 0);
+                case SOUTH -> GraphicsContext.drawImage(conveyorPointDown, 0, 0);
+            }
+
+            spaceView.getChildren().add(canvas);
         }
-        spaceView.getChildren().add(rectangle);
-        spaceView.getChildren().add(arrow);
+        catch(Exception e){
+            System.out.print("Images for Conveyor belts not in files.");
+        }
     }
 }
