a8e0988e61a83cb6773446c16d1b1a7261b7a3f3
Yusuf Kara <yusufkarasan@hotmail.com>
2022-06-08 13:34:12 +0200

    Moved the cantMoveThroughWallExeption class to its own package

--- src/main/java/dk/dtu/compute/se/pisd/roborally/Exception/cantMoveThroughWallExeption.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/Exception/cantMoveThroughWallExeption.java
@@ -1,12 +1,12 @@
-package dk.dtu.compute.se.pisd.roborally.controller;
+package dk.dtu.compute.se.pisd.roborally.Exception;
 
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import javafx.scene.control.Alert;
 
-public class cantMoveTroughWallExeption extends Exception {
+public class cantMoveThroughWallExeption extends Exception {
     private Player player;
 
-    public cantMoveTroughWallExeption(Player player) {
+    public cantMoveThroughWallExeption(Player player) {
         this.player = player;
 
         Alert hitWall = new Alert(Alert.AlertType.WARNING,
